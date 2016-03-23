7ccbc060702edacc7b066f6c1a9b81645e5af7ed
Itamar Kestenbaum <itamar@proonto.com>
2016-03-23 10:19:19 +0200

    Initial scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name okfounderApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the okfounderApp
+ */
+angular.module('okfounderApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name okfounderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the okfounderApp
+ */
+angular.module('okfounderApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
