d6f7e8d56f66cb138291aa44aa7fd8542b8e97d8
Itamar Kestenbaum <itamar@proonto.com>
2016-03-23 11:29:15 +0200

    replacing routeProvider with ui.router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,23 +13,23 @@ angular
     'ngAnimate',
     'ngCookies',
     'ngResource',
-    'ngRoute',
+    'ui.router',
     'ngSanitize',
     'ngTouch'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
+  .config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise("/");
+
+    $stateProvider
+      .state('main', {
+        url: "/",
+        templateUrl: "views/main.html",
+        controller: "MainCtrl"
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
+      .state('about', {
+        url: "/about",
+        templateUrl: "views/about.html",
+        controller: 'AboutCtrl'
       });
   });
